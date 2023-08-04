c5843718bf73386ae11ab62ebce411909a2cd704
Matias Andina <matiasandina@gmail.com>
2023-08-04 13:26:18 -0400

    adding shinyapp for mouse weights

---
+++ mouse_weights/app.R
@@ -0,0 +1,228 @@
+library(shiny)
+library(ggplot2)
+library(gridlayout)
+library(bslib)
+library(shinyFiles)
+library(shinyWidgets)
+library(tibble)
+library(DT)
+
+plot_weight_data <- function(mouse_data){
+  ggplot(
+    mouse_data,
+    aes(
+      x = datetime,
+      y = weight,
+      group = id
+    )
+  ) +
+    geom_line(alpha = 0.5) +
+    ggtitle("Mouse weight over time")
+}
+
+
+ui <- navbarPage(
+  shinyjs::useShinyjs(),
+  title = "🐁Mouse Weights",
+  selected = "Line Plots",
+  collapsible = TRUE,
+  theme = bslib::bs_theme(),
+  tabPanel(
+    title = "Line Plots",
+    grid_container(
+      layout = c(
+        "ui_in linePlots"
+      ),
+      gap_size = "16px",
+      col_sizes = c(
+        "350px",
+        "1fr"
+      ),
+      row_sizes = c(
+        "1fr"
+      ),
+      grid_card(
+        area = "ui_in",
+        card_header("Text here"),
+        card_body_fill(
+          shinyFilesButton('loadFileButton', 'Load Previous Data', title='Please choose a file', multiple=F, buttonType='default'),
+          #fileInput("loadFile", "Load Previous Data", accept = c('.csv')),
+          shinyDirButton('folder', 'Create New Data directory', 'Upload'),
+          verbatimTextOutput("folder_text"),
+          numericInput(
+            inputId = "weightInput",
+            label = "Enter Weight (gr)",
+            value = 0,
+            width = "100%"
+          ),
+          "Enter DateTime or click to select now:",
+          actionButton(
+            inputId = "nowButton",
+            label = "Now",
+            width = "20%"
+          ),
+          airDatepickerInput(
+            inputId = "dateTimeInput",
+            label = "",
+            value = Sys.time(),
+            timepicker = TRUE,
+            todayButton = FALSE,
+            update_on = "close",
+            timepickerOpts = timepickerOptions(dateTimeSeparator = " ")
+          ),
+          textInput("notes", label = "Notes:", ""),
+          actionButton("submitButton", "Submit Entry", width = "50%", 
+                       icon = icon("glyphicon glyphicon-ok", class="success")),
+          actionButton("saveButton", "Save Data", width = "50%")
+        )
+      ),
+      grid_card_plot(area = "linePlots", width = "500px") # Reduced plot width
+    )
+  ),
+  tabPanel("Data Table",
+           DTOutput("dataTable"))
+)
+
+
+server <- function(input, output, session) {
+  shinyjs::disable("submitButton")
+  # Reactive value to keep track of whether the folder has been selected
+  folderSelected <- reactiveVal(FALSE)
+  loadButtonClicked <- reactiveVal(FALSE)
+  selected_folder <- reactiveVal(NULL)
+  file_path <- reactiveVal(NULL)
+  animal_id <- reactiveVal(NULL)
+  
+  mouse_data <- reactiveVal(tibble(
+    id = character(),
+    datetime = as.POSIXct(character()),
+    weight = numeric(),
+    note = character()
+  ))
+  
+  
+  observe({
+    shinyDirChoose(input, 'folder', roots = c(home = normalizePath("~")))
+    # If a folder is selected, update the reactive value
+    if(inherits(input$folder, 'list')) {
+      folder_path <- parseDirPath(c(home = normalizePath("~")), input$folder)
+      selected_folder(folder_path)
+      folderSelected(TRUE)
+    }
+  })
+  
+  observe({
+    if(folderSelected()) {
+      shinyjs::enable("submitButton")
+    } else {
+      shinyjs::disable("submitButton")
+    }
+  })
+
+  observe({
+    selected_folder_value <- selected_folder()
+    if (!is.null(selected_folder_value)) {
+      output$folder_text <- renderText({
+        selected_folder_value
+      })
+    }
+  })
+  
+  #observeEvent(input$loadFile, {
+  #  file_path_value <- input$loadFile$datapath
+  #  full_path <- file_path_value[1]
+  #  folder_path <- dirname(full_path)
+  #  animal_id_value <- basename(folder_path)
+    
+    # Assigning values to reactive variables
+  #  file_path(file_path_value)
+  #  selected_folder(folder_path)
+  #  animal_id(animal_id_value)
+    
+  #  loaded_data <- readr::read_csv(full_path)
+  #  mouse_data(loaded_data) # Update reactive value
+    # Enable the submit button once the file is loaded
+  #  shinyjs::enable("submitButton")
+  #})
+  
+  observeEvent(input$loadFileButton, {
+    shinyFileChoose(input, 'loadFileButton', roots = c(home = normalizePath("~")), filetypes = c('csv'))
+  })
+  
+  observe({
+    if (inherits(input$loadFileButton, "list")) {
+      file_path_value <- parseFilePaths(c(home = normalizePath("~")), input$loadFileButton)
+      full_path <- file_path_value$datapath[1]
+      folder_path <- dirname(full_path)
+      animal_id_value <- basename(folder_path)
+      
+      # Assigning values to reactive variables
+      file_path(file_path_value)
+      selected_folder(folder_path)
+      animal_id(animal_id_value)
+      
+      loaded_data <- readr::read_csv(full_path)
+      mouse_data(loaded_data) # Update reactive value
+      # Enable the submit button once the file is loaded
+      shinyjs::enable("submitButton")
+    }
+  })
+  
+  # Save data
+  observeEvent(input$saveButton, {
+    folder_path <- selected_folder() 
+    animal_id <- basename(folder_path)
+    save_name <- glue::glue("sub-{animal_id}_weights.csv")
+    save_path <- file.path(folder_path, save_name)
+    readr::write_csv(mouse_data, save_path)
+    shinyalert::shinyalert("Success", paste("Data saved to", save_path), type = "success")
+  })
+  
+  observeEvent(input$nowButton, {
+    updateAirDateInput(session, "dateTimeInput", value = Sys.time())
+  })
+  
+  observeEvent(input$submitButton, {
+    # Retrieve folder_path and animal_id from reactive values
+    folder_path <- selected_folder()
+    animal_id <- animal_id()
+    # This is already POSIXct in UTC
+    datetime <- input$dateTimeInput
+    
+    # Check for duplicate datetime for this id
+    if (any(mouse_data()$id == animal_id & mouse_data()$datetime == datetime)) {
+      shinyalert::shinyalert("Error", "This datetime is already recorded for this animal. Please choose another datetime.", type = "error")
+      return()
+    }
+    
+    new_entry <- tibble(
+      id = animal_id,
+      datetime = datetime,
+      weight = input$weightInput,
+      note = input$notes
+    )
+    # Append new entry to mouse_data
+    updated_data <- dplyr::bind_rows(mouse_data(), new_entry)
+    mouse_data(updated_data) # Update reactive value
+    
+    output$dataTable <- renderDT({
+      datatable(mouse_data())
+    })
+    p <- plot_weight_data(mouse_data())
+    output$linePlots <- renderPlot({p})
+  })
+  
+  
+  output$linePlots <- renderPlot({
+    if(nrow(mouse_data()) < 2) {
+      return("Less than two datapoints; graph will not be plotted.")
+    } else {
+      plot_weight_data(mouse_data())
+    }
+
+  })
+  
+
+}
+
+shinyApp(ui, server)
